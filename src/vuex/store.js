a78f3cf877065abd17bf926571df55cd32d5c789
suvorova.m <suvorova.m@asteq.ru>
2021-03-01 01:39:04 +0300

    home

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -1,13 +1,35 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
-  state: {},
-  mutation: {},
-  actions: {},
-  getters: {}
+  state: {
+    products: [
+
+    ]
+  },
+  mutation: {
+    SET_PRODUCTS: (state : products) => {
+      state.products = products;
+    }
+  },
+  actions: {
+     GET_PRODUCTS({commit}) {
+       return axios('http://localhost:3000/products', {
+        method: "GET"
+       })
+       .then((products) =>{
+         commit('SET_PRODUCTS', products);
+       })
+     }
+  },
+  getters: {
+    PRODUCTS(state) {
+      return state.products;
+    }
+  }
 });
 
 export default store;
